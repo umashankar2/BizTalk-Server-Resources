6b6a46c955402902d3027be1347e8523c3bc2b36
umashankar2 <40573675+umashankar2@users.noreply.github.com>
2023-06-16 00:18:02 +0100

    Create host.ps1

---
+++ PowerShell-scripts/Monitor-BTS-Host-Instances-Services/host.ps1
@@ -0,0 +1,19 @@
+
+#Get DB info
+[STRING]$SQLInstance = get-wmiobject MSBTS_GroupSetting -namespace root\MicrosoftBizTalkServer | select-object -expand MgmtDbServerName
+[STRING]$BizTalkManagementDb = get-wmiobject MSBTS_GroupSetting -namespace root\MicrosoftBizTalkServer | select-object -expand MgmtDbName
+[STRING]$BizTalkGroup = "$SQLInstance" + ":" + "$BizTalkManagementDb"
+
+# Get Host instances not running
+[ARRAY]$hostInstances = get-wmiobject MSBTS_HostInstance  -namespace 'root\MicrosoftBizTalkServer' -filter '(HostType = 1 and ServiceState != 4)'
+
+#Cancel if all host instances are running
+if ($hostInstances -eq $null) {exit}
+
+Write-Host "There are  $($hostInstances.Count)  Host Instances not running in the BizTalk group $($BizTalkGroup)"
+
+
+foreach ($hostInstance in $hostInstances)
+{
+    Write-Host "$($hostInstance.Name) : $($hostInstance.HostName) : $($hostInstance.ServiceState)"
+}
