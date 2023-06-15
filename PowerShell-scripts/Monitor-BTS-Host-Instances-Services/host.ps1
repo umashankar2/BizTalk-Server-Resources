8f4b537c20dcb290999f70986c6bb71b62564c36
umashankar2 <40573675+umashankar2@users.noreply.github.com>
2023-06-16 00:41:17 +0100

    Update host.ps1

--- PowerShell-scripts/Monitor-BTS-Host-Instances-Services/host.ps1
+++ PowerShell-scripts/Monitor-BTS-Host-Instances-Services/host.ps1
@@ -5,7 +5,7 @@
 [STRING]$BizTalkGroup = "$SQLInstance" + ":" + "$BizTalkManagementDb"
 
 # Get Host instances not running
-[ARRAY]$hostInstances = get-wmiobject MSBTS_HostInstance  -namespace 'root\MicrosoftBizTalkServer'
+[ARRAY]$hostInstances = get-wmiobject MSBTS_HostInstance  -namespace 'root\MicrosoftBizTalkServer' -filter '(HostType = 1 and ServiceState = 4)'
 
 #Cancel if all host instances are running
 #if ($hostInstances -eq $null) {exit}
